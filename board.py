08d690dd90ca49c550bc987a7fb93c8739fd35f8
WilmaLiu2005 <17701248665@189.cn>
2024-11-28 17:08:38 +0800

    feat: new board and beannumber

--- board.py
+++ board.py
@@ -1,87 +1,62 @@
 import numpy as np
 import random
 
-# note: level1 9*20*20 level2 4*20*20 level3 1*20*20
+# note: level1 38*38 level2 29*29 level3 20*20
 
 def final_boardgenerator(actual_size):
+    print(actual_size)
     size = 20
-    num_blocks = actual_size // size
-    final_board = np.full((actual_size, actual_size), 2)
+    if actual_size == 20:
+        num_blocks = 1
+    else:
+        num_blocks = 2
     
-    for i in range(num_blocks):
-        for j in range(num_blocks):
-            block = boardgenerator(size, actual_size)
-            final_board[i*size:(i+1)*size, j*size:(j+1)*size] = block
+    original_board = np.full((20 * num_blocks, 20 * num_blocks), 2)
     
-    if actual_size == 60:
-        # 设置特定坐标的元素值为2(加通道)
-        coordinates = [
-            (9, 19), (9, 20),
-            (10, 19), (10, 20),
-            (29, 19), (29, 20),
-            (30, 19), (30, 20), 
-            (19, 9), (20, 9), 
-            (19, 10), (20, 10), 
-            (19, 29), (19, 30), 
-            (20, 29), (20, 30),
-            (9, 39), (9, 40),
-            (10, 39), (10, 40),
-            (19, 49), (19, 50), 
-            (20, 49), (20, 50),
-            (29, 39), (29, 40),
-            (30, 39), (30, 40), 
-            (49, 39), (49, 40),
-            (50, 39), (50, 40), 
-            (49, 19), (49, 20),
-            (50, 19), (50, 20),
-            (39, 9), (40, 9), 
-            (39, 10), (40, 10), 
-            (39, 29), (40, 29), 
-            (39, 30), (40, 30), 
-            (39, 49), (40, 49), 
-            (39, 50), (40, 50)
-        ]
-        
-        for coord in coordinates:
-            final_board[coord] = 2
+    if num_blocks == 1:
+        original_board = boardgenerator(actual_size)
+    
+    else:
+        original_board[0:20, 0:20] = boardgenerator(actual_size)
+        original_board[0:20, (size - 2):(size + 18)] = boardgenerator(actual_size)
+        original_board[(size - 2):(size + 18), 0:20] = boardgenerator(actual_size)
+        original_board[(size - 2): (size + 18), (size - 2): (size + 18)] = boardgenerator(actual_size)
+    
+    final_board = original_board[0:actual_size, 0:actual_size]
+    
+    # 在地图的边缘添加墙壁
+    final_board[0, :] = final_board[-1, :] = final_board[:, 0] = final_board[:, -1] = 0
+    
+    t = 0
+    for i in range(actual_size):
+        for j in range(actual_size):
+            if final_board[i][j] == 2 or final_board[i][j] == 3 or final_board[i][j] == 4 or final_board[i][j] == 5 or final_board[i][j] == 6 or final_board[i][j] == 7:
+                t += 1
 
-    elif actual_size == 40:
-        coordinates = [
-            (9, 19), (9, 20),
-            (10, 19), (10, 20),
-            (29, 19), (29, 20),
-            (30, 19), (30, 20), 
-            (19, 9), (20, 9), 
-            (19, 10), (20, 10), 
-            (19, 29), (19, 30), 
-            (20, 29), (20, 30)
-        ]
-        for coord in coordinates:
-            final_board[coord] = 2
-    
-    return final_board
+    return final_board, t
     
 
-def boardgenerator(size, actual_size):
+def boardgenerator(actual_size):
     # 创建20x20的二维数组，所有元素初始化为2（普通豆子）
+    size = 20
     board = np.full((size, size), 2)
 
-    # 生成墙壁
+	# 生成墙壁
     for i in range(2, size - 2, ((size - 4) // 2) + 1):
         for j in range(2, size - 2, ((size - 2) // 2) ):
             number = random.choice([1, 2, 3, 4, 5])
             if number == 1:
-                board = l_wall_generator(board, ((size - 4) // 2), min(i + ((size - 4) // 2), size - 3) - 1 , j )
+                board = l_wall_generator(board, (size - 4) // 2, i + ((size - 4) // 4) - 1, j + ((size - 4) // 4) - 1 )
             elif number == 2:
-                board = opposite_l_wall_generator(board, (size - 4) // 2, i, min(j + ((size - 4) // 2), size - 3) - 1)
+                board = opposite_l_wall_generator(board, (size - 4) // 2, i + ((size - 4) // 4) - 1, j + ((size - 4) // 4) - 1)
             elif number == 3:
                 board = cross_wall_generator(board, (size - 4) // 2, i + ((size - 4) // 4) - 1, j + ((size - 4) // 4) - 1)
             elif number == 4:
                 board = c_wall_generator(board, (size - 4) // 2, i + ((size - 4) // 4) - 1, j + ((size - 4) // 4) - 1)
             elif number == 5:
                 board = opposite_c_wall_generator(board, (size - 4) // 2, i + ((size - 4) // 4) - 1, j + ((size - 4) // 4) - 1)
-            
 	
+            
 	# 生成不同种类的豆子
     for i in range(1, size - 2):
         for j in range(1, size - 2):
@@ -97,16 +72,15 @@ def boardgenerator(size, actual_size):
                     board[i][j] = 6
                 elif number < 25:
                     board[i][j] = 7
-                elif (actual_size == 60 or actual_size == 40) and number > 75: # 空间太大，豆子太多，需要减少豆子数量
+                elif (actual_size != 20) and number > 75: # 空间太大，豆子太多，需要减少豆子数量
                     board[i][j] = 1
-     
-    # 在地图的边缘添加墙壁
-    board[0, :] = board[-1, :] = board[:, 0] = board[:, -1] = 0
     
     return board
 
-def l_wall_generator(board, size, x, y):
+def l_wall_generator(board, size, a, b):
     # 生成L形墙
+    x = a + 3
+    y = b - 3
     board[x][y] = 0
     for i in range(1, size - 2):
         board[x - i][y] = 0
@@ -124,8 +98,10 @@ def l_wall_generator(board, size, x, y):
                 cnt -= 1
     return board
 
-def opposite_l_wall_generator(board, size, x, y):
+def opposite_l_wall_generator(board, size, a, b):
     # 生成反L形墙
+    x = a - 3
+    y = b + 3
     board[x][y] = 0
     for i in range(1, size - 2):
         board[x + i][y] = 0
