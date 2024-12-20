9364d1e9414ffd0db8205aacb0879ec58932b0b5
zyc <1455854619@qq.com>
2024-12-20 15:56:29 +0800

    fix: alter scoring detalis

--- gamedata.py
+++ gamedata.py
@@ -2,14 +2,17 @@ import enum
 from dataclasses import dataclass
 import numpy as np
 
+
 class Type(enum.Enum):
-    ABNORMAL = 0 # 未正常启动
-    AI = 1 # ai
-    PLAYER = 2 # 播放器
+    ABNORMAL = 0  # 未正常启动
+    AI = 1  # ai
+    PLAYER = 2  # 播放器
+
 
 class Role(enum.Enum):
     PACMAN = 0
     GHOSTS = 1
+
 
 # 与judger交互相关数据
 FIRST_MAX_AI_TIME = 10
@@ -19,9 +22,9 @@ MAX_LENGTH = 1024
 
 # Pacman bonus
 PACMAN_HUGE_BONUS_THRESHOLD = 100
-PACMAN_HUGE_BONUS = 100
-EATEN_BY_GHOST = -20
-EAT_ALL_BEANS = 30
+PACMAN_HUGE_BONUS = 50
+EATEN_BY_GHOST = -60
+EAT_ALL_BEANS = 50
 
 HUGE_BONUS_GAMMA = [0, 0.5, 0.7, 0.9]
 ROUND_BONUS_GAMMA = 1
@@ -29,19 +32,21 @@ ROUND_BONUS_GAMMA = 1
 # Pacman skill
 DEFAULT_SKILL_TIME = [10, 10, 10]
 
+
 class Skill(enum.Enum):
     DOUBLE_SCORE = 0
     SPEED_UP = 1
     MAGNET = 2
     SHIELD = 3
 
+
 # Ghost bonus
 # NOTE: GHOST_HUGE_BONUS and PREVENT_PACMAN_EAT_ALL_BEANS will be added for all three ghosts!
 GHOST_HUGE_BONUS_THRESHOLD = 3
-GHOST_HUGE_BONUS = 20
-PREVENT_PACMAN_EAT_ALL_BEANS = 20
-EAT_PACMAN = 30
-DESTORY_PACMAN_SHIELD = 5
+GHOST_HUGE_BONUS = 50
+PREVENT_PACMAN_EAT_ALL_BEANS = 30
+EAT_PACMAN = 50
+DESTORY_PACMAN_SHIELD = 10
 
 # Portal
 PORTAL_AVAILABLE = [0, 90, 40]
@@ -54,13 +59,15 @@ MAX_LEVEL = 3  # 关卡数
 OPERATION_NUM = 5  # 操作数（上下左右不动）
 SKILL_NUM = 4
 
+
 class Direction(enum.Enum):
     STAY = 0
     UP = 1
     LEFT = 2
     DOWN = 3
     RIGHT = 4
-    
+
+
 class Update(enum.Enum):
     STAY = (0, 0)
     UP = (1, 0)
@@ -68,12 +75,14 @@ class Update(enum.Enum):
     DOWN = (-1, 0)
     RIGHT = (0, 1)
 
+
 # Board
 INITIAL_BOARD_SIZE = [0, 38, 29, 20]
 SPACE_CATEGORY = 9  # Note: 0:wall 1:empty 2:regular bean 3:bonus bean 4:speed bean 5:magnet bean 6:shield bean 7:*2 bean 8:portal
 
 PACMAN_HIT_OFFSET = -100
 GHOST_HIT_OFFSET = -200
+
 
 class Space(enum.Enum):
     WALL = 0
@@ -86,6 +95,7 @@ class Space(enum.Enum):
     DOUBLE_BEAN = 7
     PORTAL = 8
 
+
 # Event
 class Event(enum.Enum):
     # 0 and 1 should not occur simutaneously
@@ -93,7 +103,8 @@ class Event(enum.Enum):
     SHEILD_DESTROYED = 1
     # 2 and 3 should not occur simutaneously
     FINISH_LEVEL = 2
-    TIMEOUT = 3 
+    TIMEOUT = 3
+
 
 # 选手可获取的接口
 @dataclass
@@ -103,9 +114,9 @@ class GameState:
     board_size: int
     board: np.ndarray
     pacman_skill_status: list[int]
-    pacman_pos: np.ndarray[int]
-    ghosts_pos: np.ndarray[np.ndarray[int]]
+    pacman_pos: np.ndarray
+    ghosts_pos: np.ndarray
     pacman_score: int
     ghosts_score: int
     portal_available: bool
-    portal_coord: np.ndarray[int]
+    portal_coord: np.ndarray
